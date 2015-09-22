38c3dbe506faeb9307c259004b73addb5a9668b6
KiritoA <jason_dx@outlook.com>
2015-09-22 22:55:48 +0800

    第一次提交

---
+++ SerialLib/Serial.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace SerialLib
+{
+    public class Serial : SerialPort
+    {
+        SerialSettings settings;
+        private bool Listening = false;//是否没有执行完invoke相关操作
+        private bool ClosingComm = false;//是否正在关闭串口，执行Application.DoEvents，并阻止再次invoke
+        private const int rxBufSize = 128;
+        private byte[] rxBuffer = new byte[rxBufSize];
+        private int rxHead;
+        private int rxTail;
+
+        public Serial()
+        {
+            settings = new SerialSettings();
+
+            NewLine = "\r\n";
+
+            openSettings();
+        }
+
+        public void openSettings()
+        {
+            settings.ShowDialog();
+        }
+
+        public String GetSerialPortInfo()
+        {
+            if (IsOpen)
+            {
+                return "Status: " + PortName + ", " + BaudRate + ", "
+                    + Parity + ", " + DataBits + ", " + StopBits + ", " + Encoding.BodyName.ToUpper();
+            }
+            else
+                return "Status: Closed";
+        }
+
+        public bool OpenSerialPort(String portName,int baud)
+        {
+            try
+            {
+                PortName = portName;
+                BaudRate = baud;
+
+                Open();
+            }
+            catch (Exception ex)
+            {
+                
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public void CloseSerialPort()
+        {
+            ClosingComm = true;
+            while (Listening) Application.DoEvents();
+            try
+            { Close(); }
+            catch (Exception ex)
+            {
+                //捕获到异常信息，创建一个新的comm对象，之前的不能用了。 
+                //InitSerialPort();
+                //现实异常信息给客户。  
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                ClosingComm = false;
+            }
+        }
+
+        public void SerialPortWrite(byte[] buffer, int offset, int count)
+        {
+            Write(buffer, offset, count);
+        }
+
+        public int SerialPortRxAvailable()
+        {
+            return BytesToRead;
+        }
+
+        public int SerialPortRead()
+        {
+            return ReadByte();
+        }
+
+        public int SerialPortReadBuf(byte[] buffer, int count)
+        {
+            return Read(buffer, 0, count);
+        }
+
+        /*
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (ClosingComm) return;//如果正在关闭，忽略操作，直接返回，尽快的完成串口监听线程的一次循环 
+            byte ch;
+            try
+            {
+                Listening = true;//设置标记，说明我已经开始处理数据，一会儿要使用系统UI的。
+                //int n = comm.BytesToRead;//先记录下来，避免某种原因，人为的原因，操作几次之间时间长，缓存不一致
+
+                while (comm.BytesToRead > 0)
+                {
+                    ch = (byte)(comm.ReadByte() & 0xFF);
+                    rxBuffer[rxTail++] = ch;
+                    if (rxTail >= rxBufSize)
+                    {
+                        rxTail = 0;
+                    }
+                }
+
+            }
+            finally
+            {
+                Listening = false;//我用完了，ui可以关闭串口了。
+            }
+        }*/
+    }
+}
---
+++ SerialLib/SerialSettings.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace SerialLib
+{
+    partial class SerialSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_refresh = new System.Windows.Forms.Button();
+            this.comboBox_ports = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxEncoder = new System.Windows.Forms.ComboBox();
+            this.comboStopBits = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboParity = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboDataBits = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBaudRate = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_refresh);
+            this.groupBox1.Controls.Add(this.comboBox_ports);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(136, 126);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Serial Port";
+            // 
+            // button_refresh
+            // 
+            this.button_refresh.Location = new System.Drawing.Point(6, 47);
+            this.button_refresh.Name = "button_refresh";
+            this.button_refresh.Size = new System.Drawing.Size(121, 23);
+            this.button_refresh.TabIndex = 1;
+            this.button_refresh.Text = "Refresh";
+            this.button_refresh.UseVisualStyleBackColor = true;
+            this.button_refresh.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox_ports
+            // 
+            this.comboBox_ports.FormattingEnabled = true;
+            this.comboBox_ports.Location = new System.Drawing.Point(6, 19);
+            this.comboBox_ports.Name = "comboBox_ports";
+            this.comboBox_ports.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_ports.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.comboBoxEncoder);
+            this.groupBox2.Controls.Add(this.comboStopBits);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.comboParity);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.comboDataBits);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.comboBaudRate);
+            this.groupBox2.Location = new System.Drawing.Point(154, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(134, 161);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select parameters";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 50;
+            this.label6.Text = "Encoding";
+            // 
+            // comboBoxEncoder
+            // 
+            this.comboBoxEncoder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxEncoder.FormattingEnabled = true;
+            this.comboBoxEncoder.Items.AddRange(new object[] {
+            "GB2312",
+            "UTF-8"});
+            this.comboBoxEncoder.Location = new System.Drawing.Point(62, 132);
+            this.comboBoxEncoder.Name = "comboBoxEncoder";
+            this.comboBoxEncoder.Size = new System.Drawing.Size(63, 21);
+            this.comboBoxEncoder.TabIndex = 49;
+            // 
+            // comboStopBits
+            // 
+            this.comboStopBits.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboStopBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboStopBits.FormattingEnabled = true;
+            this.comboStopBits.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "1.5"});
+            this.comboStopBits.Location = new System.Drawing.Point(53, 105);
+            this.comboStopBits.Name = "comboStopBits";
+            this.comboStopBits.Size = new System.Drawing.Size(72, 21);
+            this.comboStopBits.TabIndex = 48;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 47;
+            this.label5.Text = "停止位";
+            // 
+            // comboParity
+            // 
+            this.comboParity.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboParity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboParity.FormattingEnabled = true;
+            this.comboParity.Items.AddRange(new object[] {
+            "NONE",
+            "ODD",
+            "EVEN",
+            "MARK",
+            "SPACE"});
+            this.comboParity.Location = new System.Drawing.Point(53, 49);
+            this.comboParity.Name = "comboParity";
+            this.comboParity.Size = new System.Drawing.Size(72, 21);
+            this.comboParity.TabIndex = 46;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "波特率";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "校验位";
+            // 
+            // comboDataBits
+            // 
+            this.comboDataBits.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboDataBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboDataBits.FormattingEnabled = true;
+            this.comboDataBits.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.comboDataBits.Location = new System.Drawing.Point(53, 77);
+            this.comboDataBits.Name = "comboDataBits";
+            this.comboDataBits.Size = new System.Drawing.Size(72, 21);
+            this.comboDataBits.TabIndex = 44;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 43;
+            this.label3.Text = "数据位";
+            // 
+            // comboBaudRate
+            // 
+            this.comboBaudRate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBaudRate.FormattingEnabled = true;
+            this.comboBaudRate.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "7200",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "57600",
+            "115200",
+            "128000"});
+            this.comboBaudRate.Location = new System.Drawing.Point(53, 21);
+            this.comboBaudRate.Name = "comboBaudRate";
+            this.comboBaudRate.Size = new System.Drawing.Size(72, 21);
+            this.comboBaudRate.TabIndex = 42;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(211, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Apply";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // SerialSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 222);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SerialSettings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Serial Port Settings";
+            this.Load += new System.EventHandler(this.SerialSettings_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_refresh;
+        private System.Windows.Forms.ComboBox comboBox_ports;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBoxEncoder;
+        private System.Windows.Forms.ComboBox comboStopBits;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboParity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboDataBits;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBaudRate;
+        private System.Windows.Forms.Button button2;
+    }
+}
